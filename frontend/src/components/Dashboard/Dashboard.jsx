1ba9f8ac24e541d9617fd7389fdaa36a2b5b4ecd
Uttam76Kumar <ukumar0470@gmail.com>
2026-08-18 17:32:24 +0530

    new sidebar button

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -279,6 +279,7 @@ const Dashboard = () => {
               setSidebarOpen(false);
             }}
           >
+            
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
               <line x1="18" y1="6" x2="6" y2="18"></line>
               <line x1="6" y1="6" x2="18" y2="18"></line>
